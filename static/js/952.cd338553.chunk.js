b477d8da6895bff9997e84ac38beab3b87660919
cphoover <cphoover@gmail.com>
2024-06-03 02:28:55 -0400

    Updates

--- static/js/952.cd338553.chunk.js
+++ static/js/952.cd338553.chunk.js
@@ -1,5 +1,2 @@
-"use strict";
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="f1e44514-136e-57cf-80d1-7e7443290cdb")}catch(e){}}();
-(self.webpackChunkbaltimore_food_drinks_map=self.webpackChunkbaltimore_food_drinks_map||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+"use strict";(self.webpackChunkbaltimore_food_drinks_map=self.webpackChunkbaltimore_food_drinks_map||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
 //# sourceMappingURL=952.cd338553.chunk.js.map
-//# debugId=f1e44514-136e-57cf-80d1-7e7443290cdb
